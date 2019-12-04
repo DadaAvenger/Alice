a9bd0ce64f37f5689c61c12a6851d10486595e9c
John-Andrew-TMD <372900407@qq.com>
2019-12-04 23:09:52 +0800

    242342

--- web/DadaAvenager/vue.config.js
+++ web/DadaAvenager/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: './Alice/web/DadaAvenager/dist/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: false,
