7bc8e751d7b525aa7cbaa5cf6d3f5d71bfd42516
Gavin <547249106@qq.com>
2019-12-05 15:42:48 +0800

    fix

--- web/DadaAvenager/src/api/table.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getDailyDetail(params) {
-  return request({
-    url: '/api.php?action=dailyPay&opt=getDailyPay',
-    method: 'post',
-    params
-  })
-}
